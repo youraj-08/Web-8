d0993fcd8aa31b6e635eec37a35e48a904d4ed9e
Youraj Sharma <112646420+youraj-08@users.noreply.github.com>
2023-02-24 06:03:38 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,72 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+
+const app = express();
+
+app.use(express.static("public"));                            // to access the static(local) files.(linking css)
+app.use(bodyParser.urlencoded({ extended: true }));            // Uptil here, Required almost in every project
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signup.html")
+})
+
+app.post("/", function (req, res) {
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
+
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+    const url = "https://us21.api.mailchimp.com/3.0/lists/17b8cbf6d9";
+    const options = {
+        method: "POST",
+        auth: "Youraj:2c74c2593d6f3b7756412158e633bd15-us21"
+    }
+
+    const request = https.request(url, options, function (response) {
+     
+    if(response.statusCode==200){
+        res.sendFile(__dirname + "/success.html")
+    }else{
+        res.sendFile(__dirname + "/failure.html")
+    }
+
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+    request.end();
+
+});
+
+app.post("/failure", function(req, res){
+    res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server started");
+});
+
+
+
+
+
+
+
+
